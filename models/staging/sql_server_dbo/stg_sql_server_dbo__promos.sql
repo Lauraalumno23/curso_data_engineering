ed342349e225b94e9ae69e96d8849e06e4dca18e
Laura Patricio <patriciomegias.laura362@gmail.com>
2024-05-24 17:58:21 +0000

    stage, contratos y modelos

---
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -0,0 +1,27 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+with 
+
+src_promos as (
+
+    select * from {{ source('sql_server_dbo', 'promos') }}
+
+),
+
+renamed_casted as (
+
+    select
+        promo_id,
+        discount,
+        status,
+        _fivetran_deleted,
+        _fivetran_synced
+
+    from src_promos
+
+)
+
+select * from renamed_casted
