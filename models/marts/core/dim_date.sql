580daa4d78e9bf6eb344c0f6d342a94d96f8a897
Laura Patricio <patriciomegias.laura362@gmail.com>
2024-06-18 17:50:52 +0000

    fact ventas y dimensiones

--- models/marts/core/dim_date.sql
+++ models/marts/core/dim_date.sql
@@ -1,21 +1,17 @@
-WITH scr_date AS (
-   select* from {{ ref('stg_sql_server_dbo__date') }} 
+WITH int_date AS (
+   {{ dbt_date.get_date_dimension("2020-01-01", "2030-12-31") }} 
 
-),
-renamed_casted as (
-
-    SELECT
-        DATE_ID,
-        DATE_DAY,
-        DAY_OF_WEEK,
-        DAY_OF_WEEK_NAME,
-        DAY_OF_MONTH,
-        DAY_OF_YEAR,
-        WEEK_OF_YEAR,
-        MONTH_NAME,
-        MONTH_OF_YEAR,
-        QUARTER_OF_YEAR,
-        YEAR_NUMBER
-    FROM scr_date
 )
-select * from renamed_casted
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['DATE_DAY']) }} as date_id,
+    DATE_DAY,
+    DAY_OF_WEEK,
+    DAY_OF_WEEK_NAME,
+    DAY_OF_MONTH,
+    DAY_OF_YEAR,
+    WEEK_OF_YEAR,
+    MONTH_NAME,
+    MONTH_OF_YEAR,
+    QUARTER_OF_YEAR,
+    YEAR_NUMBER
+ FROM int_date
